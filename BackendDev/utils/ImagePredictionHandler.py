c153ac6fdeedf0e14c3c99e6104a3f4b1ff08603
Xwyzworms <pratamaazmi1@gmail.com>
2023-04-20 13:24:38 +0700

    add ImagePredictionHandler

--- BackendDev/utils/ImagePredictionHandler.py
+++ BackendDev/utils/ImagePredictionHandler.py
@@ -1,11 +1,13 @@
 import numpy as np
+import PIL
+import sklearn
 
 def readImage(filename : str):
     print(f"readImage {filename}")
 
 def preprocessingImage():
     print("1. preprocessing image")
-    ...
+    
 
 def prepareModel():
     print("3.prepareModel")
